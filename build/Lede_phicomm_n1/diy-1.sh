a8f4f3853eaec5c0d792931752eac8dbff6c86ad
wgedu <49600116+wgedu@users.noreply.github.com>
2021-01-26 20:06:22 +0800

    Update diy-1.sh

--- build/Lede_phicomm_n1/diy-1.sh
+++ build/Lede_phicomm_n1/diy-1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
 
 # 基本不需要添加啥插件了,我搜集了各位大神的插件都集成一个软件包直接打入源码里面了
 # 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
