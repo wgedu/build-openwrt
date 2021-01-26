87ef8aec3b151461f32a0ba2e33e9c1ad78450d7
wgedu <49600116+wgedu@users.noreply.github.com>
2021-01-26 20:07:11 +0800

    Update diy-2.sh

--- build/Lede_phicomm_n1/diy-2.sh
+++ build/Lede_phicomm_n1/diy-2.sh
@@ -2,11 +2,6 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-sed -i '$i '"src-git helloworld https://github.com/fw876/helloworld"'' feeds.conf.default
-sed -i '$i '"src-git passwall https://github.com/xiaorouji/openwrt-passwall"'' feeds.conf.default
-sed -i '$i '"src-git OpenClash https://github.com/vernesong/OpenClash"'' feeds.conf.default
-sed -i '$i '"src-git jerrykuku https://github.com/jerrykuku/luci-app-vssr"'' feeds.conf.default
-
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
